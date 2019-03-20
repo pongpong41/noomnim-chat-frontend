eba877d9bc988a5db4d3960396e2341b03dd1134
Pongpong <pong7658@gmail.com>
2019-03-20 17:05:59 +0700

    fix-ui

--- src/app/chat/message-box/message-box.component.ts
+++ src/app/chat/message-box/message-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ElementRef, ViewChild, OnInit, Component } from '@angular/core';
 import { GroupService } from 'src/app/group.service';
 import { UserService } from 'src/app/user.service';
 import { ChatService } from 'src/app/chat.service';
@@ -8,8 +8,19 @@ import { ChatService } from 'src/app/chat.service';
   templateUrl: './message-box.component.html',
   styleUrls: ['./message-box.component.css']
 })
-export class MessageBoxComponent {
+export class MessageBoxComponent implements OnInit {
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
   constructor(public userService: UserService, public chatService: ChatService) { }
 
+  ngOnInit() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      console.log('checked');
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
 }
