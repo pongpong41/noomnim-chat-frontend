c9f2d5977935ca3203cc571177fe4829979a952a
asddsada <baipo_p@hotmail.com>
2019-11-30 13:47:42 +0700

    Finished group member logic

--- src/app/chat/group-member/group-member.component.ts
+++ src/app/chat/group-member/group-member.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { GroupService } from '../../group.service';
 import { GroupMemberService } from '../../group-member.service';
 import { User } from '../../user';
-// import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-chat-group-member',
@@ -10,29 +8,14 @@ import { User } from '../../user';
   styleUrls: ['./group-member.component.css']
 })
 export class GroupMemberComponent {
-  error = '';
-  keys = '';
+  groupId = '';
   members?: User[];
 
-  constructor(private groupService: GroupService,
-              private groupMemberService: GroupMemberService) { }
+  constructor(private groupMemberService: GroupMemberService) { }
 
   onSearchGroupMember() {
-    // this.groupService.searchGroup(this.keys).subscribe((res) => {
-    //   this.groups = res.data;
-    // });
-
-    // *** member search
+    this.groupMemberService.getMember(this.groupId).subscribe((res) => {
+      this.members = res.data;
+    });
   }
-
-  // onJoinGroup(groupId: number) {
-  //   this.groupService.joinGroup(groupId);
-  //   this.groupService.joinGroupRes.subscribe(msg => {
-  //     if (msg.data) {
-  //       this.userService.groupUpdate();
-  //     } else {
-  //       this.error = msg.error;
-  //     }
-  //   });
-  // }
 }
