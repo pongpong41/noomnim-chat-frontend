a0052ec09b189833d428ba0b589fec5548715224
asddsada <baipo_p@hotmail.com>
2019-12-06 23:20:15 +0700

    Modified group management UI

---
+++ src/app/chat/group-management/group-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from './search/search.component';
+import { GroupComponent } from './group/group.component';
+
+@Component({
+  selector: 'app-chat-group-management',
+  templateUrl: './group-management.component.html',
+  styleUrls: ['./group-management.component.css']
+})
+export class GroupManagementComponent {
+  constructor() {}
+  }
